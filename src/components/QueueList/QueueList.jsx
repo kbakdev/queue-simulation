381ff16a302603b88f0f1e5e9883fd73aa7b83db
Adam Cichowski <adam.cichowski98@outlook.com>
2023-01-21 15:10:48 +0100

    feat: add person to queue

--- src/components/QueueList/QueueList.jsx
+++ src/components/QueueList/QueueList.jsx
@@ -1,150 +1,65 @@
-import React from "react";
-import {parseInteger} from "jsdom/lib/jsdom/living/helpers/strings.js";
+import React, { useEffect, useState } from "react";
+import { parseInteger } from "jsdom/lib/jsdom/living/helpers/strings.js";
 import QueueTable from "../QueueTable/QueueTable.jsx";
+import { useConfigurationContext } from "../../context/ConfigurationContext.jsx";
 
 // Generate random number between min and max
 function generateRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// Find Queue with the lowest sum and return index
-function findQueueWithTheLowestSumByKey() {
-    const queueSum = document.getElementById('sum');
-    const queueSumArray = queueSum.innerHTML.split(" ");
-
-    let lowestSum = parseInteger(queueSumArray[0]);
-    let lowestSumKey = 0;
-
-    for (let i = 1; i < queueSumArray.length; i++) {
-        if (parseInteger(queueSumArray[i]) < lowestSum) {
-            lowestSum = parseInteger(queueSumArray[i]);
-            lowestSumKey = i;
-        }
-    }
-
-    console.log(lowestSumKey);
-    return lowestSumKey;
-}
-
-function makeQueueArray(howMuchQueues) {
-    const queue = document.getElementById('queue');
-    const queueArray = queue.innerHTML.split(" ");
-
-    // parse div queue to array
-    for (let i = 0; i < queueArray.length; i++) {
-        queueArray[i] = queueArray[i].split(",");
-    }
-
-    console.log(queueArray);
-}
-
-function countQueues() {
-    const queue = document.getElementById('queue');
-    const queueArray = queue.innerHTML.split(",");
-    console.log(queueArray.length);
-    return queueArray.length;
-}
-
-// Generate random person to queue with the lowest sum which is saved
-function addToQueueWithLowestSum() {
-    const min = parseInteger(localStorage.getItem("min"));
-    const max = parseInteger(localStorage.getItem("max"));
-    const randomPerson = generateRandomNumber(min, max);
-
-    const queueSum = document.getElementById('sum');
-    const queueSumArray = queueSum.innerHTML.split(" ");
-
-    const queue = document.getElementById('queue');
-    const queueArray = queue.innerHTML.split(" ");
-
-    // parse div queue to array
-    for (let i = 0; i < queueArray.length; i++) {
-        queueArray[i] = queueArray[i].split(",");
-    }
-
-    const lowestSumKey = findQueueWithTheLowestSumByKey();
-
-    queueArray[lowestSumKey].push(randomPerson);
-    queueSumArray[lowestSumKey] = parseInteger(queueSumArray[lowestSumKey]) + randomPerson;
-
-    queueSum.innerHTML = queueSumArray.join(" ");
-    queue.innerHTML = queueArray.join(" ");
-
-    // if first element is empty, then delete it
-    if (queueArray[lowestSumKey][0] === "") {
-        queueArray[lowestSumKey].shift();
-        // remove , from div id queue
-        queue.innerHTML = queueArray.join(" ");
-    }
-
-    console.log(queueArray);
-}
-
-// Check which queue has the lowest sum
-function findQueueWithTheLowestSum() {
-    const queueSum = document.getElementById('sum');
-    const queueSumArray = queueSum.innerHTML.split(" ");
-
-    let lowestSum = parseInteger(queueSumArray[0]);
-    let lowestSumKey = 0;
-
-    for (let i = 1; i < queueSumArray.length; i++) {
-        if (parseInteger(queueSumArray[i]) < lowestSum) {
-            lowestSum = parseInteger(queueSumArray[i]);
-            lowestSumKey = i;
-        }
-    }
-
-    console.log(lowestSumKey);
-    return lowestSumKey;
-}
-
-// Decrease sum and first number from array of every queue by 1
-function removeFromQueue() {
-    const queueSum = document.getElementById('sum');
-    const queueSumArray = queueSum.innerHTML.split(" ");
-
-    const queue = document.getElementById('queue');
-    const queueArray = queue.innerHTML.split(" ");
-
-    // parse div queue to array
-    for (let i = 0; i < queueArray.length; i++) {
-        queueArray[i] = queueArray[i].split(",");
-    }
-
-    for (let i = 0; i < queueArray.length; i++) {
-        queueSumArray[i] = parseInteger(queueSumArray[i]) - 1;
-        console.log(queueArray[i]);
-        // get first element from array, if it's 0 then shift it, if not then decrease it by 1
-        if (parseInteger(queueArray[i][0]) === 1) {
-            queueArray[i].shift();
-        } else {
-            queueArray[i][0] = parseInteger(queueArray[i][0]) - 1;
-        }
-    }
-
-    queueSum.innerHTML = queueSumArray.join(",");
-    queue.innerHTML = queueArray.join(",");
-}
+const getKeyLowestQueue = (queues) => {
+  const sumAllQueue = Object.entries(queues).map(([_, queue]) =>
+    queue.reduce((prev, num) => Number(prev) + Number(num), 0)
+  );
+  const lowestQueue = Math.min(...sumAllQueue);
+  const keyLowestQueue = sumAllQueue.indexOf(lowestQueue);
+  console.log({ lowestQueue, keyLowestQueue, sumAllQueue });
+  return keyLowestQueue === -1 ? 0 : keyLowestQueue;
+};
 
 function QueueList() {
-    return (
-        <>
-            <div>
-                <button onClick={addToQueueWithLowestSum}>Add new person to queue</button>
-                <button onClick={removeFromQueue}>Remove from queue</button>
-                <button onClick={findQueueWithTheLowestSumByKey}>Find queue with the lowest sum</button>
-                <button onClick={findQueueWithTheLowestSum}>findQueueWithTheLowestSum</button>
-                <button onClick={makeQueueArray}>Make queue array</button>
-                <button onClick={countQueues}>Count queues</button>
-            </div>
-            <table>
-                <ol>
-                    <QueueTable/>
-                </ol>
-            </table>
-        </>
-    );
+  const { config } = useConfigurationContext();
+  const [queue, setQueue] = useState(
+    Object.fromEntries(
+      Array.from({ length: config.numberOfQueues }, (_, i) => [i, []])
+    )
+  );
+  console.log(queue);
+  const handlerAddToQueue = () => {
+    const keyLowestQueue = getKeyLowestQueue(queue);
+    setQueue((prev) => {
+      console.log(prev, "prev", prev[keyLowestQueue], keyLowestQueue);
+      return {
+        ...prev,
+        [keyLowestQueue]: [
+          ...prev[keyLowestQueue],
+          generateRandomNumber(config.min, config.max),
+        ],
+      };
+    });
+  };
+  return (
+    <>
+      <div>
+        <button onClick={handlerAddToQueue}>Add new person to queue</button>
+        <button onClick={removeFromQueue}>Remove from queue</button>
+        <button onClick={findQueueWithTheLowestSumByKey}>
+          Find queue with the lowest sum
+        </button>
+        <button onClick={findQueueWithTheLowestSum}>
+          findQueueWithTheLowestSum
+        </button>
+        <button onClick={makeQueueArray}>Make queue array</button>
+        <button onClick={countQueues}>Count queues</button>
+      </div>
+      <table>
+        <ol>
+          <QueueTable />
+        </ol>
+      </table>
+    </>
+  );
 }
 
 export default QueueList;
