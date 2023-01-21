044a30d232d0325dcf409520023e0d19cf2deb00
Adam Cichowski <adam.cichowski98@outlook.com>
2023-01-21 21:37:32 +0100

    feat: deacrese queue

--- src/components/QueueList/QueueList.jsx
+++ src/components/QueueList/QueueList.jsx
@@ -5,17 +5,28 @@ import { useConfigurationContext } from "../../context/ConfigurationContext.jsx"
 
 // Generate random number between min and max
 function generateRandomNumber(min, max) {
-  return Math.floor(Math.random() * (parseInteger(max) - parseInteger(min) + 1)) + parseInteger(min)
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
+const decreaseAllQueues = (queues) => {
+  const decreasedQueues = Object.entries(queues).map(([key, queue]) => {
+    const newQueue = queue.map((person, i) => {
+      if (i === 0 && person > 0) {
+        return person - 1;
+      }
+      return person;
+    });
 
+    const filteredZeroValues = newQueue.filter((person) => person > 0);
+    return filteredZeroValues;
+  });
+  return Object.fromEntries(decreasedQueues.map((value, key) => [key, value]));
+};
 const getKeyLowestQueue = (queues) => {
-  console.log(queues, "xd");
   const sumAllQueue = Object.entries(queues).map(([_, queue]) =>
     queue.reduce((prev, num) => Number(prev) + Number(num), 0)
   );
   const lowestQueue = Math.min(...sumAllQueue);
   const keyLowestQueue = sumAllQueue.indexOf(lowestQueue);
-  console.log({ lowestQueue, keyLowestQueue, sumAllQueue });
   return keyLowestQueue === -1 ? 0 : keyLowestQueue;
 };
 
@@ -29,14 +40,18 @@ function QueueList() {
         )
   );
   console.log(queue, "stan", config);
+  const handlerReduceAllQueue = () => {
+    setQueue((queues) => decreaseAllQueues(queues));
+  };
   const handlerAddToQueue = () => {
     const keyLowestQueue = getKeyLowestQueue(queue);
+    console.log(generateRandomNumber(config.min, config.max), config);
     setQueue((prev) => {
       return {
         ...prev,
         [keyLowestQueue]: [
           ...prev[keyLowestQueue],
-          generateRandomNumber(config.min, config.max),
+          generateRandomNumber(Number(config.min), Number(config.max)),
         ],
       };
     });
@@ -46,6 +61,7 @@ function QueueList() {
     <>
       <div>
         <button onClick={handlerAddToQueue}>Add new person to queue</button>
+        <button onClick={handlerReduceAllQueue}>Reduce queues</button>
       </div>
 
       <ol>
